bd7cc375c0831e03ceff3db003ce7f5f27339466
songjinghe <songjinghe@users.noreply.github.com>
2024-11-02 13:23:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,26 +4,17 @@ from st_table_select_cell import st_table_select_cell
 
 st.subheader("Example of st_table_select_cell")
 
-# prepare an example dataframe
 data = pd.DataFrame({'Dataset':['energy','traffic','syn'], 'Test':['ehistory','snapshot','aggmax'], 'PG': [3,6,9], 'TG':[2,5,7]})
 st.dataframe(data)
 
-# show table and get user selected cell
 selectedCell = st_table_select_cell(data)
 st.write(selectedCell)
 
 if selectedCell:
     rowId = selectedCell['rowId']
     colIndex = selectedCell['colIndex']
-    st.write(rowId, colIndex)
-
-    # return column name of the selected cell.
-    st.write(data.columns[colIndex])
-    
-    # return row of the selected cell as dict.
-    st.write(data.iloc[int(rowId)].to_dict())
-    
-    # return cell content as string.
-    st.write(data.iat[int(rowId), colIndex])
+    st.info('cell "{}" selected at row {} and col {} ({})'.format(
+        data.iat[int(rowId), colIndex], rowId, colIndex, data.columns[colIndex]))
+    st.write('selected row data: ', data.iloc[int(rowId)].to_dict())
 else:
-    st.write('no select')
+    st.warning('no select')
